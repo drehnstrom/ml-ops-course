028422f839023290821b6eca5f3b916d6cb6ac48
drehnstrom <drehnstrom@users.noreply.github.com>
2022-01-24 08:58:07 -0500

    Update run-pets-job-on-dataflow.sh

--- dataflow-demo/run-pets-job-on-dataflow.sh
+++ dataflow-demo/run-pets-job-on-dataflow.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 # Don't forget to add your Project ID and Bucket Name below
-export PROJECT=[YOUR-PROJECT_ID]
 export BUCKET=[YOU-BUCKET-NAME]
 export REGION=us-central1
 
@@ -31,5 +30,5 @@ python pets-job.py --input gs://$BUCKET/pets.txt \
 --output gs://$BUCKET/pets_job_results_out \
 --temp_location gs://$BUCKET/tmp/ \
 --region us-central1 \
---project $PROJECT \
+--project $GOOGLE_CLOUD_PROJECT \
 --runner DataflowRunner
